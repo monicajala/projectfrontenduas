5189a6a510dc6b7b773544ab851ee98344ac45c9
amnnic <amnnic07@gmail.com>
2024-12-05 16:41:51 +0700

    perbaikannnn

--- projectfrontenduas/wishlistscript.js
+++ projectfrontenduas/wishlistscript.js
@@ -4,7 +4,7 @@ const app = angular.module('wishlistApp', []);
 // Controller for wishlist
 app.controller('WishlistController', function ($scope) {
     // Initialize wishlist
-    $scope.wishlist = ["Want to Bali"];
+    $scope.wishlist = ["Want to go to Bali"];
 
     // Add new item to the wishlist
     $scope.addWishlist = function () {
