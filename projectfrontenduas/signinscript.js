c62b38e2b4ba3a3a79a4b1de4cc62f2b0989de73
Maria Melisa H <mmelisaher@gmail.com>
2024-12-05 12:58:39 +0700

    perubahan pada sign in script

--- projectfrontenduas/signinscript.js
+++ projectfrontenduas/signinscript.js
@@ -1,30 +1,38 @@
-// script.js
-angular.module('authApp', [])
-.controller('AuthController', function($scope) {
-    // Default ke halaman Sign In
-    $scope.currentPage = 'signin';
-    $scope.formData = {};
+var app = angular.module('authApp', []);
 
-    // Navigasi antar halaman
-    $scope.navigateTo = function(page) {
+app.controller('AuthController', ['$scope', '$http', function ($scope, $http) {
+    $scope.currentPage = 'signin'; // Default halaman adalah signin
+    $scope.formData = {}; // Data form yang akan dikirim
+
+    // Fungsi untuk berpindah halaman
+    $scope.navigateTo = function (page) {
         $scope.currentPage = page;
     };
 
-    // Fungsi dummy Sign In
-    $scope.signIn = function() {
-        if ($scope.formData.email && $scope.formData.password) {
-            alert('Signed In with Email: ' + $scope.formData.email);
-        } else {
-            alert('Please fill in all fields.');
-        }
+    // Fungsi Sign Up
+    $scope.signUp = function () {
+        console.log('Sign Up data:', $scope.formData);
+        $http.post('http://localhost:3000/api/auth/signup', $scope.formData) // Update URL ke '/api/auth/signup'
+            .then(function (response) {
+                alert('Sign Up successful!');
+                $scope.navigateTo('signin'); // Pindah ke halaman signin setelah signup
+            })
+            .catch(function (error) {
+                console.error('Sign Up error:', error);
+                alert('Failed to Sign Up. ' + error.data.error); // Tambahkan pesan error lebih detail
+            });
     };
 
-    // Fungsi dummy Sign Up
-    $scope.signUp = function() {
-        if ($scope.formData.firstName && $scope.formData.lastName && $scope.formData.email && $scope.formData.password) {
-            alert('Account created for Email: ' + $scope.formData.email);
-        } else {
-            alert('Please fill in all fields.');
-        }
+    // Fungsi Log In
+    $scope.signIn = function () {
+        console.log('Sign In data:', $scope.formData);
+        $http.post('http://localhost:3000/api/auth/signin', $scope.formData) // Update URL ke '/api/auth/signin'
+            .then(function (response) {
+                alert('Log In successful!');
+            })
+            .catch(function (error) {
+                console.error('Log In error:', error);
+                alert('Failed to Log In. ' + error.data.error); // Tambahkan pesan error lebih detail
+            });
     };
-});
+}]);
